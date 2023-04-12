1ad348af4f11e870f4e5642bf0891aab58e512cc
hokageCV <chaitanyavaru@gmail.com>
2023-04-12 16:29:14 +0530

    update swagger docs

--- src/model/user.ts
+++ src/model/user.ts
@@ -12,17 +12,45 @@ import { TimeSlotSchema } from "./timeslot";
  *         name:
  *           type: string
  *           description: The name of the user
+ *           example: John Doe
  *         email:
  *           type: string
  *           description: The email address of the user
+ *           example: john.doe@example.com
  *         password:
  *           type: string
  *           description: The password of the user
+ *           example: p@ssw0rd
  *         slot:
  *           type: array
- *           items:
- *             $ref: '#/components/schemas/TimeSlot'
  *           description: The time slots booked by the user
+ *           items:
+ *             type: object
+ *             properties:
+ *               start:
+ *                 type: string
+ *                 format: date-time
+ *                 description: The start time of the time slot
+ *                 example: '2023-04-12T18:00:00.000Z'
+ *               end:
+ *                 type: string
+ *                 format: date-time
+ *                 description: The end time of the time slot
+ *                 example: '2023-04-12T19:00:00.000Z'
+ *           example:
+ *             - start: '2023-04-12T18:00:00.000Z'
+ *               end: '2023-04-12T19:00:00.000Z'
+ *             - start: '2023-04-14T16:00:00.000Z'
+ *               end: '2023-04-14T17:00:00.000Z'
+ *       example:
+ *         name: John Doe
+ *         email: john.doe@example.com
+ *         password: p@ssw0rd
+ *         slot:
+ *           - start: '2023-04-12T18:00:00.000Z'
+ *             end: '2023-04-12T19:00:00.000Z'
+ *           - start: '2023-04-14T16:00:00.000Z'
+ *             end: '2023-04-14T17:00:00.000Z'
  */
 
 interface UserSchemaType {
