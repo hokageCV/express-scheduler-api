e1143577d7c0aa70c97d2314f956f609804f21f7
hokageCV <chaitanyavaru@gmail.com>
2023-04-12 23:24:28 +0530

    update readme

--- dist/model/inputForGetTimeSlot.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.inputForGetTimeSlot = void 0;
-const zod_1 = require("zod");
-exports.inputForGetTimeSlot = zod_1.z.object({
-    body: zod_1.z.object({
-        hostEmail: zod_1.z.string().email(),
-    }),
-});
